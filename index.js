47d03b41ab60c6b86529c24a92f18ea964e00dac
shadracknyaribo <stilespulsar77@gmail.com>
2022-12-08 10:07:15 -0800

    added post functionality

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+
+
+function submitdata (name,email){
+
+
+fetch("http://localhost:3000/users", {
+  method: "post",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  
+
+  //make sure to serialize your JSON body
+  body: JSON.stringify({
+    name: name,
+    email: email
+  })
+})
+
+.then(function (response)  { 
+    return response.json();
+})
+.then(function (object) {
+    console.log(object);
+  })
+.catch(function (error) {
+    alert("Faulty data");
+    console.log(error.message);
+  });
+  return submitdata;
+}
+
+
+
+
+
+
